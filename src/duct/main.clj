3cd365f0ef619e4e2fb49372776bebe1880cf295
James Reeves <jreeves@weavejester.com>
2023-11-23 16:42:57 +0000

    Add default init-key method
    Adds a method that looks up a function that matches the key. This
    allows bare functions to be used for init-key.

--- src/duct/main.clj
+++ src/duct/main.clj
@@ -1,6 +1,16 @@
 (ns duct.main
   (:require [integrant.core :as ig]))
 
+(defn- default-init-fn [k]
+  (let [sym (symbol k)]
+    (require (symbol (namespace sym)))
+    (if-let [v (find-var sym)]
+      (var-get v)
+      (throw (ex-info (str "No such var: " sym) {:var sym})))))
+
+(defmethod ig/init-key :default [k v]
+  ((default-init-fn k) v))
+
 (defn -main [& _args]
   (-> (slurp "duct.edn")
       (ig/read-string)
