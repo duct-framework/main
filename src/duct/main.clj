15e6d05f2910eba967a5afd299519a5a59edeaa6
James Reeves <jreeves@weavejester.com>
2023-11-29 16:15:50 +0000

    Add environment variable support

--- src/duct/main.clj
+++ src/duct/main.clj
@@ -1,5 +1,6 @@
 (ns duct.main
-  (:require [integrant.core :as ig]))
+  (:require [clojure.walk :as walk]
+            [integrant.core :as ig]))
 
 (defn- default-init-fn [k]
   (let [sym (symbol k)]
@@ -11,9 +12,23 @@
 (defmethod ig/init-key :default [k v]
   ((default-init-fn k) v))
 
+(defn- get-env-var [sym {:keys [default]}]
+  (or (System/getenv (name sym)) default))
+
+(defn- bind [coll sym opts]
+  (let [val (get-env-var sym opts)]
+    (walk/postwalk #(if (= sym %) val %) coll)))
+
+(defn- bind-vars [{:keys [vars] :as config}]
+  (update config :system #(reduce-kv bind % vars)))
+
+(defn- init [{:keys [system]}]
+  (-> (doto system ig/load-namespaces)
+      (ig/prep)
+      (ig/init)))
+
 (defn -main [& _args]
   (-> (slurp "duct.edn")
       (ig/read-string)
-      (doto ig/load-namespaces)
-      (ig/prep)
-      (ig/init)))
+      (bind-vars)
+      (init)))
