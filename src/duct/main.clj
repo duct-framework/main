019e60ba70f76129bb8b6d4f5332ac1be60db199
James Reeves <jreeves@weavejester.com>
2024-10-12 19:50:12 +0100

    Convert duct.main for new Integrant features

--- src/duct/main.clj
+++ src/duct/main.clj
@@ -1,42 +1,12 @@
 (ns duct.main
-  (:require [clojure.walk :as walk]
-            [clojure.tools.cli :as cli]
+  (:require [clojure.tools.cli :as cli]
             [integrant.core :as ig]))
 
-(defn- default-init-fn [k]
-  (let [sym (symbol k)]
-    (require (symbol (namespace sym)))
-    (if-let [v (find-var sym)]
-      (var-get v)
-      (throw (ex-info (str "No such var: " sym) {:var sym})))))
-
-(defmethod ig/init-key :default [k v]
-  ((default-init-fn k) v))
-
-(defn- deep-merge [a b]
-  (merge-with #(if (and (map? %1) (map? %2))
-                 (deep-merge %1 %2)
-                 %2)
-              a b))
-
-(defn- merge-profiles [{:keys [profiles] :as config} profile-keys]
-  (reduce #(deep-merge %1 (profiles %2))
-          (dissoc config :profiles)
-          profile-keys))
-
-(defn- get-env-var [sym {:keys [default]}]
-  (or (System/getenv (name sym)) default))
-
-(defn- bind [coll sym opts]
-  (let [val (get-env-var sym opts)]
-    (walk/postwalk #(if (= sym %) val %) coll)))
-
-(defn- bind-vars [{:keys [vars] :as config}]
-  (update config :system #(reduce-kv bind % vars)))
-
-(defn- init [{:keys [system]}]
+(defn- init [{:keys [system]} profiles]
+  (ig/load-hierarchy)
   (-> (doto system ig/load-namespaces)
-      (ig/prep)
+      (ig/expand (ig/deprofile profiles))
+      (ig/deprofile profiles)
       (ig/init)))
 
 (defn- parse-concatenated-keywords [s]
@@ -57,6 +27,4 @@
       (print-help opts)
       (-> (slurp "duct.edn")
           (ig/read-string)
-          (merge-profiles (-> opts :options :profiles))
-          (bind-vars)
-          (init)))))
+          (init (-> opts :options :profiles))))))
