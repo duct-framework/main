13a4c0ba7a2231642ea28f04ce571853f1edb2aa
James Reeves <jreeves@weavejester.com>
2024-10-15 03:00:21 +0100

    Allow vars to be set via argument flags

--- src/duct/main.clj
+++ src/duct/main.clj
@@ -1,20 +1,24 @@
 (ns duct.main
   (:require [clojure.tools.cli :as cli]
+            [clojure.string :as str]
             [integrant.core :as ig]))
 
-(defn- var-value [{:keys [env default]}]
-  (or (System/getenv (str env)) default))
+(defn- var-value [{:keys [env arg default]} opts]
+  (or (opts (keyword arg))
+      (System/getenv (str env))
+      default))
 
-(defn- resolve-vars [params]
-  (into {} (reduce-kv #(assoc %1 %2 (var-value %3)) {} params)))
+(defn- resolve-vars [vars opts]
+  (into {} (reduce-kv #(assoc %1 %2 (var-value %3 opts)) {} vars)))
 
-(defn- init [{:keys [system vars]} profiles]
-  (ig/load-hierarchy)
-  (-> (doto system ig/load-namespaces)
-      (ig/expand (ig/deprofile profiles))
-      (ig/deprofile profiles)
-      (ig/bind (resolve-vars vars))
-      (ig/init)))
+(defn- init [{:keys [system vars]} {:keys [profiles] :as opts}]
+  (let [opts (dissoc opts :profiles :help)]
+    (ig/load-hierarchy)
+    (-> (doto system ig/load-namespaces)
+        (ig/expand (ig/deprofile profiles))
+        (ig/deprofile profiles)
+        (ig/bind (resolve-vars vars opts))
+        (ig/init))))
 
 (defn- halt-on-shutdown [system]
   (.addShutdownHook (Runtime/getRuntime) (Thread. #(ig/halt! system))))
@@ -22,21 +26,29 @@
 (defn- parse-concatenated-keywords [s]
   (map keyword (re-seq #"(?<=:).*?(?=:|$)" s)))
 
-(def cli-options
+(def default-cli-options
   [["-p" "--profiles PROFILES" "A concatenated list of profile keys"
     :parse-fn parse-concatenated-keywords]
    ["-h" "--help"]])
+
+(defn- var->cli-option [{:keys [arg doc]}]
+  (when arg
+    `[nil
+      ~(str "--" arg " " (str/upper-case arg))
+      ~@(when doc [doc])]))
+
+(defn- cli-options [{:keys [vars]}]
+  (into default-cli-options (keep var->cli-option) (vals vars)))
 
 (defn- print-help [{:keys [summary]}]
   (println "Usage:\n\tclj -M:duct")
   (println (str "Options:\n" summary)))
 
 (defn -main [& args]
-  (let [opts (cli/parse-opts args cli-options)]
+  (let [config (ig/read-string (slurp "duct.edn"))
+        opts   (cli/parse-opts args (cli-options config))]
     (if (-> opts :options :help)
       (print-help opts)
-      (let [profiles (-> opts :options :profiles)]
-        (-> (slurp "duct.edn")
-            (ig/read-string)
-            (init profiles)
-            (doto halt-on-shutdown))))))
+      (-> config
+          (init (:options opts))
+          (doto halt-on-shutdown)))))
