94f4e965ee24e039db62a4caed97cbe1c5cdb5f1
James Reeves <jreeves@weavejester.com>
2024-11-03 00:19:54 +0000

    Make test-project into web app

--- test-project/src/test_project.clj
+++ test-project/src/test_project.clj
@@ -2,4 +2,10 @@
   (:require [duct.logger :as log]))
 
 (defn hello [{:keys [logger name]}]
-  (log/info logger ::greet {:name name}))
+  (fn [_request]
+    (log/info logger ::greet {:name name})
+    {:status 200
+     :headers {"Content-Type" "text/html; charset=UTF-8"}
+     :body (str "<!DOCTYPE html>\n<html lang=\"en\">"
+                "<head><title>Greet Example</title></head>"
+                "<body><h1>Hello " name "</h1></body></html>")}))
