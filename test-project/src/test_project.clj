b285767370f454a9b8be468447e6b69ca7fd392a
James Reeves <jreeves@weavejester.com>
2024-11-15 05:47:29 +0000

    Add SQL database to test project

--- test-project/src/test_project.clj
+++ test-project/src/test_project.clj
@@ -1,11 +1,22 @@
 (ns test-project
-  (:require [duct.logger :as log]))
+  (:require [next.jdbc :as jdbc]))
 
-(defn hello [{:keys [logger name]}]
-  (fn [_request]
-    (log/info logger ::greet {:name name})
-    {:status 200
-     :headers {"Content-Type" "text/html; charset=UTF-8"}
-     :body (str "<!DOCTYPE html>\n<html lang=\"en\">"
-                "<head><title>Greet Example</title></head>"
-                "<body><h1>Hello " name "</h1></body></html>")}))
+(def ^:const inc-counter
+  "INSERT INTO counters (remote_addr) VALUES (?)
+   ON CONFLICT (remote_addr) DO UPDATE SET counter = counter + 1")
+
+(def ^:const get-counter
+  "SELECT counter FROM counters WHERE remote_addr = ?")
+
+(defn hello [{:keys [db name]}]
+  (fn [{:keys [remote-addr]}]
+    (let [[{counter :counters/counter}]
+          (jdbc/with-transaction [tx db]
+            (jdbc/execute! tx [inc-counter remote-addr])
+            (jdbc/execute! tx [get-counter remote-addr]))]
+      {:status 200
+       :headers {"Content-Type" "text/html; charset=UTF-8"}
+       :body (str "<!DOCTYPE html>\n<html lang=\"en\">"
+                  "<head><title>Greet Example</title></head>"
+                  "<body><h1>Hello " name " (count: " counter ")"
+                  "</h1></body></html>")})))
