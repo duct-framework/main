8e1c8b95deae307e488d8583bc981ea3dc0c8784
James Reeves <jreeves@weavejester.com>
2024-11-22 01:15:39 +0000

    Sync deps.edn with project.clj

--- project.clj
+++ project.clj
@@ -3,8 +3,10 @@
   :url "https://github.com/duct-framework/main"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.11.4"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
                  [org.clojure/tools.cli "1.1.230"]
-                 [integrant "0.13.0"]
+                 [integrant "0.13.1"]
                  [integrant/repl "0.4.0"]
-                 [com.openvest/repl-balance "0.2.114"]])
+                 [com.openvest/repl-balance "0.2.114"]
+                 [nrepl "1.3.0"]
+                 [cider/cider-nrepl "0.50.2"]])
