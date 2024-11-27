ead073d10c762bbb356a7bdd07356aeca21a415b
James Reeves <jreeves@weavejester.com>
2024-11-27 05:49:36 +0000

    Replace Balance REPL -main with custom function

--- src/duct/main/repl.clj
+++ src/duct/main/repl.clj
@@ -1,8 +1,14 @@
 (ns duct.main.repl
-  (:require [duct.main.config :as config]
+  (:require [clojure.main :as main]
+            [clojure.repl :as repl]
+            [duct.main.config :as config]
             [integrant.core :as ig]
             [integrant.repl :as igrepl]
-            [repl-balance.clojure.main :as balance]))
+            [repl-balance.core :as bal-core]
+            [repl-balance.clojure.line-reader :as clj-line-reader]
+            [repl-balance.clojure.main :as bal-main]
+            [repl-balance.clojure.service.local :as clj-service]
+            [repl-balance.jline-api :as jline]))
 
 (defn- setup-user-ns [load-config options]
   (in-ns 'user)
@@ -14,6 +20,24 @@
   (.addShutdownHook (Runtime/getRuntime)
                     (Thread. (resolve 'igrepl/halt))))
 
+(defn- handle-sigint-form []
+  `(let [thread# (Thread/currentThread)]
+     (repl/set-break-handler! (fn [_signal#] (.stop thread#)))))
+
+(defn- start-repl []
+  (bal-core/ensure-terminal
+   (bal-core/with-line-reader
+     (clj-line-reader/create
+      (clj-service/create
+       (when jline/*line-reader* @jline/*line-reader*)))
+     (binding [*out* (jline/safe-terminal-writer jline/*line-reader*)]
+       (println "[Repl balance] Type :repl/help for online help info")
+       (main/repl
+        :eval   (fn [form] (eval `(do ~(handle-sigint-form) ~form)))
+        :print  bal-main/syntax-highlight-prn
+        :prompt (fn [])
+        :read   (bal-main/create-repl-read))))))
+
 (defn create-repl [load-config options]
   (setup-user-ns load-config options)
-  balance/-main)
+  start-repl)
