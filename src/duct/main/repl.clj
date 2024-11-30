89e9c31b99125793f605885d21b180cebbbc389f
James Reeves <jreeves@weavejester.com>
2024-11-30 16:19:24 +0000

    Add sync-deps to REPL refers

--- src/duct/main/repl.clj
+++ src/duct/main/repl.clj
@@ -15,6 +15,7 @@
 (defn- setup-user-ns [load-config options]
   (in-ns 'user)
   (require '[clojure.repl :refer [apropos dir doc find-doc pst source]])
+  (require '[clojure.repl.deps :refer [sync-deps]])
   (require '[integrant.repl :refer [clear go halt prep init reset reset-all]])
   (require '[integrant.repl.state :refer [config system]])
   (igrepl/set-prep! #(-> (load-config)
