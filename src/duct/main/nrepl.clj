4a6a45164f6ea5c0edf7babf7a8b53f7cf9aa5b5
James Reeves <jreeves@weavejester.com>
2025-03-31 23:38:18 +0100

    Add explicit stop for nREPL server on exit

--- src/duct/main/nrepl.clj
+++ src/duct/main/nrepl.clj
@@ -3,6 +3,12 @@
             [nrepl.cmdline :as cli]
             [nrepl.server :as nrepl]))
 
+(defn stop-nrepl-on-shutdown [server]
+  (.addShutdownHook (Runtime/getRuntime)
+                    (Thread. (bound-fn []
+                               (term/verbose "Stopping nREPL server")
+                               (nrepl/stop-server server)))))
+
 (defn- nrepl-handler [{:keys [cider]}]
   (if cider
     (requiring-resolve 'cider.nrepl/cider-nrepl-handler)
@@ -10,6 +16,6 @@
 
 (defn start-nrepl [options]
   (let [server (nrepl/start-server {:handler (nrepl-handler options)})]
-    (term/verbose (str "Started NREPL server on port " (:port server)))
+    (term/verbose (str "Started nREPL server on port " (:port server)))
     (cli/save-port-file server {})
-    server))
+    (doto server stop-nrepl-on-shutdown)))
