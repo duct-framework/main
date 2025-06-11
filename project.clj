86868444a4bb08a7cbe8f0a8d93437f8b318bdbb
James Reeves <jreeves@weavejester.com>
2025-06-11 01:41:44 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/duct-framework/main"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.12.0"]
+  :dependencies [[org.clojure/clojure "1.12.1"]
                  [org.clojure/tools.cli "1.1.230"]
                  [integrant "0.13.1"]
                  [integrant/repl "0.4.0"]
                  [com.openvest/repl-balance "0.2.114"]
                  [dev.weavejester/hashp "0.4.0"]
                  [nrepl "1.3.1"]
-                 [cider/cider-nrepl "0.53.2"]])
+                 [cider/cider-nrepl "0.56.0"]])
