13036437882fa061aa7d56cf3ca7eb380ee3c26f
James Reeves <jreeves@weavejester.com>
2025-09-25 02:13:39 +0100

    Fix custom doc function in Cider dir-locals.el

--- resources/duct/main/dir-locals.el
+++ resources/duct/main/dir-locals.el
@@ -1,4 +1,5 @@
 ((nil
   (cider-jack-in-cmd . "clojure -M:duct --nrepl --cider")
   (cider-ns-refresh-before-fn . "integrant.repl/suspend")
-  (cider-ns-refresh-after-fn  . "integrant.repl/resume")))
+  (cider-ns-refresh-after-fn  . "integrant.repl/resume")
+  (cider-repl-init-code . ("(ns user)"))))
